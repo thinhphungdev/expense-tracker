a348697eb4b5c25e82a42eaac59f49e2932b7a72
thinhphungdev <thinhphung.contact@gmail.com>
2021-10-02 13:47:47 +0700

    first setup

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './App.css';
+
+import Header from './components/Header.js';
+import Balance from './components/Balance.js';
+import IncomeExpense from './components/IncomeExpense.js';
+import Transaction from './components/Transaction';
+
+import AddTransaction from './components/AddTransaction';
+
+function App() {
+    return (
+        <React.Fragment>
+            <Header />
+            <div className="container">
+                <Balance />
+                <IncomeExpense />
+                <Transaction />
+            </div>
+            <AddTransaction />
+        </React.Fragment>
+    )
+}
+
+export default App;
---
+++ src/components/Balance.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Balance() {
+    return (
+        <>
+          <h4>Income</h4>
+          <p id="money-plus" className="money plus">+$0.00</p>   
+        </>
+    )
+}
+
+export default Balance
